adc55ee814c81ff79770d4ca1e21bef4f51de84b
Sunny <mdalifaiad@gmail.com>
2026-09-07 00:49:33 +0900

    Updates

---
+++ _next/static/chunks/pages/FontProvider-aad280b096e87efb.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[885],{7448:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/FontProvider",function(){return u(577)}])}},function(n){n.O(0,[888,774,179],function(){return n(n.s=7448)}),_N_E=n.O()}]);
